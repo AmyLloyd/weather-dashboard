c55135f725a58828418c25d76355ec98644aeb6e
Create-88 <141834742+Create-88@users.noreply.github.com>
2023-11-03 15:07:46 +1030

    collect searchItem details and save in a searchLocations array

--- assets/script.js
+++ assets/script.js
@@ -1,53 +1,50 @@
+var submitButton = document.getElementById('submit');
 
-//search input pastes elements below dynamically
-
-var searchFormEl = $('#search-form');
-var locationListEl = $('#search-list');
-
-// create function to handle form submission
-
-
-function handleFormSubmit(event) {
+submitButton.addEventListener("click", function(event) {
   event.preventDefault();
 
-  // select form element by its `name` attribute and get its value
-  searchItem = $('input[name="search-input"]').val();
+  var citySearchInput= document.querySelector("#search-city-input").value;
+  var countrySearchInput = document.querySelector("#search-country-input").value;
   
-
-  // if there's nothing in the form entered, don't print to the page
-  if (!searchItem) {
-    console.log('No location entered!');
+  if(!citySearchInput) {
+    console.log("You need a search input value!");
     return;
   }
-
-  // print to the page
- locationListEl.append('<li>' + searchItem + '</li>');
-
- getApi(searchItem);
  
-  // clear the form input element
-  $('input[name="search-input"]').val('');
+  //Uses search input to build the query string for LATITUDE AND LONGITUDE of location
+  var requestCoordinatesUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + citySearchInput + ',' + countrySearchInput + '&limit=1&appid=427b6e060e396ad75184ba7b9f4658e6'
+  console.log(requestCoordinatesUrl);
+  console.log(citySearchInput);
+  console.log(countrySearchInput);
+  
+  //fetches coordinates for search Item and pushes them to a searchLocations array
+  let searchLocations = [];
 
-}
+  fetch(requestCoordinatesUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json(); // Convert the response body to JSON
+  })
+  .then(data => {
+    // Assign the response data to the outer variable
+    console.log(data);
+    let responseCoordinatesData = JSON.stringify(data);
+    console.log(responseCoordinatesData);
+    searchLocations.push(responseCoordinatesData);
+    console.log(searchLocations);
+    // You can now access 'responseData' outside the fetch scope
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
 
-// Create a submit event listener on the form element
-searchFormEl.on('submit', handleFormSubmit);
+  //Append an element with searchLocation data
 
-//search API for the city name and return data
+  //Take coordinates from searchLocation for weather fetch
+  
 
-//use API to get coordinates for city may need to add ,{state code},{country code}
-function getApi(searchItem) {
-    var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + searchItem + '&limit=5&appid=427b6e060e396ad75184ba7b9f4658e6';
-    
-    fetch(requestUrl)
-    .then(function (response) {
-        if(response.ok) {
-            response.json().then(function (data) {
-                console.log(data);
-            });
-        } else {
-            alert('Error: ' + response.statusText);
-        }
-    })
-};
+ 
+});
 
